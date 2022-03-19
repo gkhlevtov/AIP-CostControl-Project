74149a61d7d8e3829c69c0ffd1832af260269797
gkhlevtov <ghkhlevtov@mail.ru>
2022-03-19 18:13:06 +0200

    Update cost classes

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, render_template, abort, request
 from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
 from werkzeug import exceptions
-from os import environ
 from datetime import datetime
 from costs import user_costs
 
 currentTime = datetime.now()
 
 app = Flask(__name__)
-# app.secret_key = environ['APP_SECRET_KEY']
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -19,17 +17,17 @@ class UserCost(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(80), nullable=False)
     cover = db.Column(db.Text)
-    items = db.relationship('TodoItem', backref='list', lazy=True)
+    items = db.relationship('CostItem', backref='list', lazy=True)
 
     def __repr__(self):
-        return f'<TodoList {self.name}>'
+        return f'<UserCost {self.name}>'
 
 
 class CostItem(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.Text, nullable=False)
-    is_done = db.Column(db.Boolean, nullable=False, default=False)
-    list_id = db.Column(db.Integer, db.ForeignKey('todo_list.id'), nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    value = db.Column(db.Integer, nullable=False)
+    cost_id = db.Column(db.Integer, db.ForeignKey('user_cost.id'), nullable=False)
 
 
 @app.route('/')
@@ -47,8 +45,8 @@ def userpage():
 
 @app.route('/user-costs/<int:cost_id>')
 def get_costs(cost_id):
-    """user_cost = UserCost.query.get(cost_id)
-    print(user_cost)"""
+    user_cost = UserCost.query.get(escape(cost_id))
+    print(user_cost)
     if cost_id > len(user_costs) or cost_id <= 0:
         abort(exceptions.NotFound.code)
     return render_template('list.html', user_cost=user_costs[cost_id - 1])
@@ -63,7 +61,6 @@ def search():
             if text in item['title'].lower():
                 selected_costs.append(user_cost)
             continue
-    print(selected_costs)
     return render_template('index.html', user_costs=selected_costs)
 
 
