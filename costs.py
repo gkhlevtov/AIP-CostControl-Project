e473ec86c25b42db3735595d7550d2f3d05b5402
gkhlevtov <ghkhlevtov@mail.ru>
2022-03-19 16:30:40 +0200

    Add costs temporary file

---
+++ costs.py
@@ -0,0 +1,224 @@
+user_costs = [
+    {
+        'id': "1",
+        'name': 'январь',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '4200'
+            },
+            {
+                'title': 'Бензин', 'value': '1000'
+            },
+            {
+                'title': 'Одежда', 'value': '6890'
+            },
+            {
+                'title': 'Игры', 'value': '3452'
+            },
+            {
+                'title': 'Гачи мерч', 'value': '300'
+            }
+        ]
+    },
+    {
+        'id': "2",
+        'name': 'февраль',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '4200'
+            },
+            {
+                'title': 'Бензин', 'value': '1000'
+            },
+{
+                'title': 'Арбуз', 'value': '480'
+            },
+            {
+                'title': 'Одежда', 'value': '6890'
+            },
+            {
+                'title': 'ЖКХ', 'value': '3350'
+            },
+            {
+                'title': 'Игры', 'value': '6523'
+            },
+        ]
+    },
+    {
+        'id': "3",
+        'name': 'март',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Вода 0.5 л', 'value': '17'
+            },
+            {
+                'title': 'Шоколадка', 'value': '1000'
+            },
+            {
+                'title': 'Игры', 'value': '3000'
+            },
+        ]
+    },
+    {
+        'id': "4",
+        'name': 'апрель',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '4200'
+            },
+            {
+                'title': 'Бензин', 'value': '1000'
+            },
+            {
+                'title': 'Одежда', 'value': '6890'
+            },
+            {
+                'title': 'ЖКХ', 'value': '3350'
+            },
+            {
+                'title': 'Игры', 'value': '13499'
+            },
+        ]
+    },
+    {
+        'id': "5",
+        'name': 'май',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '4200'
+            },
+            {
+                'title': 'Одежда', 'value': '6890'
+            },
+        ]
+    },
+    {
+        'id': "6",
+        'name': 'июнь',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '4200'
+            },
+            {
+                'title': 'Мороженое', 'value': '500'
+            },
+            {
+                'title': 'ЖКХ', 'value': '3350'
+            },
+        ]
+    },
+    {
+        'id': "7",
+        'name': 'июль',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '5000'
+            },
+            {
+                'title': 'Плавки', 'value': '2500'
+            },
+{
+                'title': 'Сланцы', 'value': '1300'
+            },
+{
+                'title': 'Очки', 'value': '800'
+            },
+        ]
+    },
+    {
+        'id': "8",
+        'name': 'август',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '4200'
+            },
+            {
+                'title': 'Карандаши', 'value': '100'
+            },
+        ]
+    },
+    {
+        'id': "9",
+        'name': 'сентябрь',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '3400'
+            },
+            {
+                'title': 'Бензин', 'value': '2000'
+            },
+            {
+                'title': 'ЖКХ', 'value': '3350'
+            },
+        ]
+    },
+    {
+        'id': "10",
+        'name': 'октябрь',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '4200'
+            },
+            {
+                'title': 'Бензин', 'value': '1000'
+            },
+            {
+                'title': 'Одежда', 'value': '6890'
+            },
+            {
+                'title': 'ЖКХ', 'value': '3350'
+            },
+            {
+                'title': 'Игры', 'value': '13499'
+            },
+        ]
+    },
+    {
+        'id': "11",
+        'name': 'ноябрь',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '4200'
+            },
+            {
+                'title': 'Бензин', 'value': '1000'
+            },
+            {
+                'title': 'Одежда', 'value': '6890'
+            },
+            {
+                'title': 'ЖКХ', 'value': '3350'
+            },
+            {
+                'title': 'Игры', 'value': '13499'
+            },
+        ]
+    },
+    {
+        'id': "12",
+        'name': 'декабрь',
+        'cover': 'images/diagrams/example.png',
+        'items': [
+            {
+                'title': 'Еда', 'value': '200'
+            },
+            {
+                'title': 'Одежда', 'value': '4000'
+            },
+            {
+                'title': 'Игры', 'value': '5000'
+            },
+        ]
+    }
+]
